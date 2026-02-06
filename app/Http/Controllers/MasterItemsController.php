c4173b66fd80caa3ed88fef81027bd48acdd84fb
tomflutter <radeatomy3@gmail.com>
2026-02-06 13:55:29 +0700

    update

--- app/Http/Controllers/MasterItemsController.php
+++ app/Http/Controllers/MasterItemsController.php
@@ -23,7 +23,13 @@ class MasterItemsController extends Controller
 
         if (!empty($kode)) $data_search = $data_search->where('kode', $kode);
         if (!empty($nama)) $data_search = $data_search->where('nama', 'LIKE', '%' . $nama . '%');
-        if (!empty($hargamin)) $data_search = $data_search->where('harga_beli', '>=', $hargamin)->where('harga_beli', '<=', $hargamax);
+        if (!empty($hargamin)) {
+            $data_search = $data_search->where('harga_beli', '>=', $hargamin);
+        }
+        // DISNI RUBAH
+        if (!empty($hargamax)) {
+            $data_search = $data_search->where('harga_beli', '<=', $hargamax);
+        }
 
         $data_search = $data_search->select('kode', 'nama', 'jenis', 'harga_beli', 'laba', 'supplier')->orderBy('id')->get();
 
@@ -33,6 +39,7 @@ class MasterItemsController extends Controller
             'data' => $data_search
         ]);
     }
+
 
     public function formView($method, $id = 0)
     {
