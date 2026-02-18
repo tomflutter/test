5dbc007f93dc5ab1507184c8673ed6ba5d369fc5
tomflutter <radeatomy3@gmail.com>
2026-02-18 13:11:48 +0700

    12345

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,7 +3,6 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use App\Models\User;
 
 return new class extends Migration
 {
@@ -23,12 +22,6 @@ return new class extends Migration
             $table->rememberToken();
             $table->timestamps();
         });
-
-	$user = new User;
-	$user->email = "123@123";
-	$user->name = "123";
-	$user->password = Hash::make("123");
-	$user->save();
     }
 
     /**
